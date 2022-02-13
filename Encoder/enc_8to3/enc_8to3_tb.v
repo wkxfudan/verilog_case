1f37c1fcb2c727534c5b04e7ecab0392a9ec4c24
0xtaruhi <zhang_zhengyi@outlook.com>
2022-02-13 18:44:37 +0800

    Style Rename Encoder83 & Priority Encoder83

--- Encoder/enc_8to3/enc_8to3_tb.v
+++ Encoder/enc_8to3/enc_8to3_tb.v
@@ -1,17 +1,55 @@
-module enc_8to3 (
-    input wire a0,
-    input wire a1,
-    input wire a2,
-    input wire a3,
-    input wire a4,
-    input wire a5,
-    input wire a6,
-    input wire a7,
-    output wire [2:0] out
-);
+module enc_8to3_tb;
 
-    assign out[2] = a7 | a6 | a5 | a4;
-    assign out[1] = a3 | a2 | a7 | a6;
-    assign out[0] = a3 | a1 | a7 | a5;
+    reg  a0;
+    reg  a1;
+    reg  a2;
+    reg  a3;
+    reg  a4;
+    reg  a5;
+    reg  a6;
+    reg  a7;
+    wire [2:0] out;
+    integer idx = 0;
 
-endmodule //enc_4to2
+
+    `define TEST_CASE(VAR, VALUE) \
+        idx = idx + 1; \
+        if(VAR == VALUE) $display("Case %d passed!", idx); \
+        else begin $display("Case %d failed!", idx); $finish; end 
+
+    enc_8to3 dut (
+        .a0(a0),
+        .a1(a1),
+        .a2(a2),
+        .a3(a3),
+        .a4(a4),
+        .a5(a5),
+        .a6(a6),
+        .a7(a7),
+        .out(out)
+    );
+
+    initial begin
+        {a7, a6, a5, a4, a3, a2, a1, a0} = 8'b0000_0000;
+        #5 `TEST_CASE(out, 3'b000);
+        {a7, a6, a5, a4, a3, a2, a1, a0} = 8'b0000_0001;
+        #5 `TEST_CASE(out, 3'b000);
+        {a7, a6, a5, a4, a3, a2, a1, a0} = 8'b0000_0010;
+        #5 `TEST_CASE(out, 3'b001);
+        {a7, a6, a5, a4, a3, a2, a1, a0} = 8'b0000_0100;
+        #5 `TEST_CASE(out, 3'b010);
+        {a7, a6, a5, a4, a3, a2, a1, a0} = 8'b0000_1000;
+        #5 `TEST_CASE(out, 3'b011);
+        {a7, a6, a5, a4, a3, a2, a1, a0} = 8'b0001_0000;
+        #5 `TEST_CASE(out, 3'b100);
+        {a7, a6, a5, a4, a3, a2, a1, a0} = 8'b0010_0000;
+        #5 `TEST_CASE(out, 3'b101);
+        {a7, a6, a5, a4, a3, a2, a1, a0} = 8'b0100_0000;
+        #5 `TEST_CASE(out, 3'b110);
+        {a7, a6, a5, a4, a3, a2, a1, a0} = 8'b1000_0000;
+        #5 `TEST_CASE(out, 3'b111);
+        $display("Success!");
+        $finish;
+    end
+
+endmodule
