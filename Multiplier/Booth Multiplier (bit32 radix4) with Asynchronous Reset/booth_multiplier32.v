84bda9a2beff4e6e5a4cd629c1680d9a99410208
Adnepukan <20300750044@fudan.edu.cn>
2022-02-19 19:24:45 +0800

    Add Booth Multiplier

---
+++ Multiplier/Booth Multiplier (bit32 radix4) with Asynchronous Reset/booth_multiplier32.v
@@ -0,0 +1,76 @@
+`timescale 1ns/1ps
+
+module booth_multiplier32 
+    (
+        input wire clk,
+        input wire async_rst,
+        input wire valid,
+        input wire [31:0] A,
+        input wire [31:0] B,
+        output wire [63:0] R,
+        output wire ready
+    );
+    
+    // load and store A
+    reg [31:0] A_reg;
+    always @(posedge clk or posedge async_rst)
+        if(async_rst)
+            A_reg<=32'b0;
+        else if(valid&ready)
+            A_reg<=A;
+        else 
+            A_reg<=A_reg;
+
+    // load and store PART-SUM,B
+    reg [65:0] M;
+    wire [34:0] PARTSUM_load;
+    always @(posedge clk or posedge async_rst)
+        if(async_rst)
+            M<=66'b0;
+        else if(valid&ready)
+            M<={33'b0,B,1'b0};
+        else if((~valid)&ready)
+            M<=M;
+        else
+            M<={PARTSUM_load,M[32:2]};
+    
+    // calculate PART-SUM, analyse M[2:0]
+    wire [34:0] A_ext;
+    assign A_ext={A_reg[31],A_reg[31],A_reg[31],A_reg};
+    
+    wire [34:0] M_ext;
+    assign M_ext={M[65],M[65],M[65:33]};
+    
+    wire [34:0] A_op;
+    assign A_op={35{(M[2:0]==3'b001)|(M[2:0]==3'b010)}} & A_ext                     |
+                {35{(M[2:0]==3'b011)}}                  & {A_ext[33:0],1'b0}        |
+                {35{(M[2:0]==3'b100)}}                  & {~A_ext[33:0]+1'b1,1'b0}  |
+                {35{(M[2:0]==3'b101)|(M[2:0]==3'b110)}} & (~A_ext+1'b1)             ;
+
+    assign PARTSUM_load=M_ext+A_op;
+
+    // get RESULT
+    assign R=M[64:1];
+    
+    reg start;
+    always @(posedge clk or posedge async_rst)
+        if(async_rst)
+            start<=0;
+        else
+            start<=valid&ready;
+
+    reg [3:0] count;
+    always @(posedge clk or posedge async_rst)
+        if(async_rst)
+            count<=4'd0;
+        else if(start)
+            count<=4'd1;
+        else if(count==5'd15)
+            count<=4'd0;
+        else if(count==4'd0)
+            count<=count;
+        else
+            count<=count+4'd1;
+
+    assign ready=(~(|count))&(~start);
+endmodule
---
+++ Multiplier/Booth Multiplier (bit32 radix4) with Asynchronous Reset/booth_multiplier32_tb.v
@@ -0,0 +1,99 @@
+`timescale 1ns / 1ps
+
+// Simulation time: 810s
+module booth_multiplier32_tb;
+
+	// Inputs
+	reg clk;
+	reg async_rst;
+	reg valid;
+	reg signed [31:0] A;
+	reg signed [31:0] B;
+	reg [30:0] A_data;
+	reg [30:0] B_data;
+
+	// Outputs
+	wire [63:0] R;
+	wire ready;
+
+	// Instantiate the Unit Under Test (UUT)
+	booth_multiplier32 uut 
+		(
+			.clk(clk), 
+			.async_rst(async_rst), 
+			.valid(valid), 
+			.A(A), 
+			.B(B), 
+			.R(R), 
+			.ready(ready)
+		);
+
+	// Initial signals
+	initial
+		fork
+			clk=0;
+			async_rst=0;
+			valid=0;
+			A=0;
+			B=0;
+			#1 async_rst=1;
+			#3 async_rst=0;
+			#4 A_data={$random}%(2**31);
+			#4 A={1'b0,A_data};
+			#4 B_data={$random}%(2**31);
+			#4 B={1'b0,B_data};
+			#4 valid=1;
+			#9 valid=0;
+			#204 A_data={$random}%(2**31);
+			#204 A={1'b0,A_data};
+			#204 B_data={$random}%(2**31);
+			#204 B={1'b1,B_data};
+			#204 valid=1;
+			#209 valid=0;
+			#404 A_data={$random}%(2**31);
+			#404 A={1'b1,A_data};
+			#404 B_data={$random}%(2**31);
+			#404 B={1'b0,B_data};
+			#404 valid=1;
+			#409 valid=0;
+			#604 A_data={$random}%(2**31);
+			#604 A={1'b1,A_data};
+			#604 B_data={$random}%(2**31);
+			#604 B={1'b1,B_data};
+			#604 valid=1;
+			#609 valid=0;
+			#810 $stop;
+		join
+
+	// Clock signal
+	always #5 clk=~clk;
+	
+	// Auto check
+	wire signed [63:0] Mul;
+    assign Mul=A*B;
+
+	integer cycle;
+	initial cycle=1;
+	always #200
+		cycle<=cycle+1;
+
+	reg result;
+	initial 
+		begin
+			result=1;
+			#805;
+			if(result)
+				$display("SIMULATION SUCCEEDED!");
+			else	
+				$display("SIMULATION FAILED!");
+		end
+	always #200
+		if(Mul==R)
+			$display("%ds: passed!",200*cycle);
+		else
+			fork
+				$display("%ds: Something wrong!",200*cycle);
+				result=0;
+			join
+endmodule
+
